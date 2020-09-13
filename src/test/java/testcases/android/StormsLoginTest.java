e90d82bc5d7086cd69be938d8198781e33f9773b
saifsms91 <saifsms91@rediffmail.com>
2020-09-13 21:05:01 +0800

    Code Refactor

--- src/test/java/testcases/android/StormsLoginTest.java
+++ src/test/java/testcases/android/StormsLoginTest.java
@@ -1,6 +1,7 @@
 
 package testcases.android;
 
+import static java.lang.Thread.sleep;
 import static org.testng.Assert.assertEquals;
 
 import io.appium.java_client.AppiumDriver;
@@ -8,6 +9,7 @@ import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.touch.offset.PointOption;
 import io.appium.java_client.TouchAction;
+import org.openqa.selenium.Dimension;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -15,64 +17,24 @@ import base.TestBase;
 import io.qameta.allure.Description;
 import screens.androidpageobjects.LandingPageAndroid;
 
+//import java.time.Duration;
+
+
 public class StormsLoginTest extends TestBase {
     public LandingPageAndroid LandingPage = new LandingPageAndroid();
     public static AppiumDriver driver2;
 
     @Test(priority = 1, enabled = true)
-    @Description("Verify Valid login for Amazon")
+    @Description("Verify Valid login for Storm")
     public void verifyValidLogin() throws Exception {
 
-        /*System.out.println("verifyValidLogin: " + driver);
-        MobileElement swipeEl = (MobileElement) driver.findElement(MobileBy.id("list_view"));
-        int startx = swipeEl.getLocation().getX() + 5;
-        int starty = swipeEl.getCenter().getY();
-        int endx = swipeEl.getSize().getWidth() - 10;
-        int endy = 0;*/
-        LandingPage.tapAccount();
-//        LandingPage.enterUsername();
-//        LandingPage.enterPassword();
-//        LandingPage.storm_in1();
 
-        LandingPage.login();
-
-//    new TouchAction(driver).longPress(250, 1200).moveTo(900, 1200).release().perform();
-
-        //new TouchAction(driver2).press(56,1299).moveTo(356,1310).release().perform();
-        //new TouchAction().press(el0).moveTo(el1).release()
-
-        System.out.println("touchabove: ");
-
-//        try{
-//           // TouchAction touchAction = new TouchAction(driver);
-//           // touchAction.press(PointOption.point(10, 20)).moveTo(PointOption.point(200, 200)).release().perform();
-//            System.out.println("touchabove: ");
-//            Thread.sleep(10000);
-//            new TouchAction(driver).press(startx, starty).waitAction(Duration.ofMillis(100)).moveTo(endx, endy).waitAction(Duration.ofMillis(100)).release().perform();
-//
-//            // TouchAction touchAction = new TouchAction(driver);
-//            // touchAction.press(PointOption.point(10, 20)).moveTo(PointOption.point(200, 200)).release().perform();
-//            System.out.println("touchabove siuccess: ");
-//            sleep(10);
-//
-//        }
-//        catch(InterruptedException ie){
-//            System.out.println("touchabove: "+ ie);
-//        }
-
-
-
-
-
-        System.out.println("beloq: ");
-
-        //touchAction.longPress(FirstElement, Duration.ofSeconds(15)).moveTo(endx,
-              //  endy).release().perform();
-
-        //new TouchAction(driverObject).tap(PointOption.point(356,1310)).perform();
-
-//        String SIGNOUTTEXT = LandingPage.logOut();
-//        assertEquals(SIGNOUTTEXT, "SIGN OUT");
+       LandingPage.tapAccount();
+       LandingPage.login();
+       System.out.println("beloq: ");
+       LandingPage.hamBurger();
+       String USERNAMETEXT = LandingPage.userNameText();
+       assertEquals(USERNAMETEXT, "@saif");
 
 
 
