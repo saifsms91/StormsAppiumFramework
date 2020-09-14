86f620995f02ecb8c0e546c85f2c40df2a1d3db2
saifsms91 <saifsms91@rediffmail.com>
2020-09-14 15:19:21 +0800

    Set absolute path

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -2,6 +2,7 @@ package utils;
 
 public class Constants {
 
-    public static final String ANDROID_URI = "/Users/saif.siddiqui/Downloads/Storms_Task/src/test/resources/app/Storms_0_2_5_778_Prod.apk";
+    public static final String ANDROID_URI = "./src/test/resources/app/Storms_0_2_5_778_Prod.apk";
+
 
 }
